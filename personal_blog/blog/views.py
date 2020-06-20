1ae2ed686e1c0e9119d11eeaffcbb91a64ad8a32
kevinnguyen <kevinnguyen@iMac.local>
2020-06-20 08:10:04 -0700

    Add Home and About Page

--- personal_blog/blog/views.py
+++ personal_blog/blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
+posts = [
+    {
+        'author': 'KevinNguyen',
+        'title': 'Test Post',
+        'content': 'Testing Blog Post Content',
+        'date_posted': '20 June 2020'
+    },
+    {
+        'author': 'KevinNguyen',
+        'title': 'Test Post 2',
+        'content': 'Testing Blog Post Content 2',
+        'date_posted': '20 June 2020'
+    },
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home<h1>')
+    context = {
+        'posts': posts,
+        'title': 'Home'
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About<h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
